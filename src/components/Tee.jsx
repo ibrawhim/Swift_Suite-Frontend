3d88afb1810afbee760169d0b759f0f43a13dfa8
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2024-01-30 10:38:04 +0100

    tee test

---
+++ src/components/Tee.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tee = () => {
+  return (
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis excepturi unde aperiam alias, amet animi suscipit sed veniam autem earum? Amet optio delectus molestiae inventore suscipit vel unde quod minima?
+    </div>
+  )
+}
+
+export default Tee
