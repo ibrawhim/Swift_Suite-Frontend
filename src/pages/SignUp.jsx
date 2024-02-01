7408472de112b16644cba9ade661fe105f94c27d
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2024-02-01 15:47:06 +0100

    making signup comp.

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import signImage from '../Images/signup.svg'
 import Navbar from '../components/Navbar'
+import { IoEyeSharp } from "react-icons/io5";
+import { BsEyeSlashFill } from "react-icons/bs";
+
 
 const SignUp = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+    const [confirmVisible, setConfirmVisible] = useState(false);
+
+    const togglePasswordVisibility = (field) => {
+      if (field === 'password') {
+          setPasswordVisible(!passwordVisible);
+      } else if (field === 'confirm') {
+          setConfirmVisible(!confirmVisible);
+      }
+  };
+
   return (
       <div>
         <div>
@@ -18,23 +32,25 @@ const SignUp = () => {
               <p className='flex my-2 font-semibold text-xl'>Sign Up</p>
                 <div className='my-2'>
                   <label htmlFor="">Name</label><br />
-                  <input type="text" placeholder='Jane Doe' className='py-2 w-full border-2  border-[#089451]'/>
+                  <input type="text" placeholder='Jane Doe' className='px-4 py-2 w-full border-2  border-[#089451]'/>
                 </div>
                 <div className='my-2'>
                   <label htmlFor="Email">Email</label><br />
-                  <input type="text" placeholder='jane@gmail.com' className='py-2 w-full border-2 border-[#089451]'/>
+                  <input type="text" placeholder='Jane@gmail.com' className='px-4 py-2 w-full border-2 border-[#089451]'/>
                 </div>
                 <div className='my-2'>
                   <label htmlFor="">Username</label><br />
-                  <input type="text" placeholder='jane123' className='py-2 w-full border-2 border-[#089451]'/>
+                  <input type="text" placeholder='Jane1234'  className='px-4 py-2 w-full border-2 border-[#089451]'/>
                 </div>
-                <div className='my-2'>
+                <div className='my-2 relative'>
                   <label htmlFor="">Password</label><br />
-                  <input type="text" placeholder='********' className='py-2 w-full border-2 border-[#089451]'/>
+                  <input  type={passwordVisible ? 'text' : 'password'}  autoComplete='off' className='border-2 border-[#089451] py-2 px-4 w-full'/>
+                  <span onClick={() => togglePasswordVisibility('password')} className='absolute top-[35px] right-1'>{!passwordVisible ? <BsEyeSlashFill /> : <IoEyeSharp />}</span>
                 </div>
-                <div className='my-2'>
+                <div className='my-2 relative'>
                   <label htmlFor="">Confirm Password</label><br />
-                  <input type="text" placeholder='********' className='py-2 w-full border-2 border-[#089451]'/>
+                  <input type={confirmVisible ? 'text' : 'password'}  className='border-2 bg-white border-[#089451] py-2 px-4 w-full'/>
+                  <span onClick={() => togglePasswordVisibility('confirm')} className='absolute top-[35px] right-1'>{!confirmVisible ? <BsEyeSlashFill /> : <IoEyeSharp />}</span>
                 </div>
                 <button className='w-full bg-[#089451] text-white font-bold py-2 my-2'>Sign Up</button>
               </form>
