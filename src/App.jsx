2f24f629c4fbad156c26381f490630697d570019
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2024-02-20 16:34:09 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Rootlayout from './layout/Rootlayout'
 import SignUp from './pages/SignUp'
 import ErrorPage from './pages/Errorpage'
 import Signin from './pages/Signin'
+import Reset from './pages/Reset'
+import EnterEmail from './pages/EnterEmail'
 
 
 
@@ -19,6 +21,8 @@ function App() {
       <Route path='/signin' element={<Signin/>}/>
       <Route path='layout/*' element={<Rootlayout/>}/>
       <Route path="*" element={<ErrorPage/>}/>
+      <Route path='/reset'  element={<Reset/>}/>
+      <Route path='/enteremail' element={<EnterEmail/>}/>
     </Routes>
   </>
   )
